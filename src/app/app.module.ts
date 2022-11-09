bedaa9858252749dd29ee1cfd8b0d062ef43e57a
Marius Pageot <marius.pageot@gmail.com>
2022-11-09 10:14:53 +0100

    toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgChartsModule, NgChartsConfiguration } from 'ng2-charts';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgChartsModule, NgChartsConfiguration} from 'ng2-charts';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatOptionModule, MatRippleModule} from "@angular/material/core";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    NgChartsModule
+    NgChartsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [
-    { provide: NgChartsConfiguration, useValue: { generateColors: false }}
+    {provide: NgChartsConfiguration, useValue: {generateColors: false}}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
