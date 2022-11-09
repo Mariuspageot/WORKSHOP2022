b7122b050c599ccada207c15b7a2412059e52da6
Rose <rose.heautot@gmail.com>
2022-11-09 23:52:58 +0100

    utilisation des ressources

--- src/app/Components/conso-energie-regions/conso-energie-regions.component.ts
+++ src/app/Components/conso-energie-regions/conso-energie-regions.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {ChartConfiguration, ChartEvent, ChartType} from 'chart.js';
+import {ChartConfiguration, ChartType} from 'chart.js';
 import {BaseChartDirective} from 'ng2-charts';
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
@@ -32,6 +32,10 @@ export class ConsoEnergieRegionsComponent implements OnInit {
       datalabels: {
         anchor: 'end',
         align: 'end'
+      },
+      title:{
+        display: true,
+        text: "Consommation des deux régions par année"
       }
     }
   };
@@ -57,28 +61,4 @@ export class ConsoEnergieRegionsComponent implements OnInit {
       }
     ]
   };
-
-  // events
-  public chartClicked({event, active}: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public chartHovered({event, active}: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40];
-
-    this.chart?.update();
-  }
-
 }
--- src/app/Components/type-energie-regions/type-energie-regions.component.ts
+++ src/app/Components/type-energie-regions/type-energie-regions.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {BaseChartDirective} from "ng2-charts";
+import {ChartConfiguration, ChartData, ChartType} from "chart.js";
+import DatalabelsPlugin from 'chartjs-plugin-datalabels';
+import colors from "../../../assets/colors.json"
 
 @Component({
   selector: 'app-type-energie-regions',
-  templateUrl: './type-energie-regions.component.html',
-  styleUrls: ['./type-energie-regions.component.scss']
+  templateUrl: './type-energie-regions.component.html'
 })
-export class TypeEnergieRegionsComponent implements OnInit {
+export class TypeEnergieRegionsComponent {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit(): void {
   }
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public pieChartType: ChartType = 'pie';
+  public pieChartPlugins = [DatalabelsPlugin];
+
+  // Pie
+  public pieChartOptionsRegion1: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      title:{
+        display: true,
+        text:"Utilisation des ressources en Pays de la Loire"
+      }
+    }
+  };
+  public pieChartOptionsRegion2: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      title:{
+        display: true,
+        text:"Utilisation des ressources en Nouvelle aquitaine"
+      }
+    }
+  };
+  public pieChartDataRegion1: ChartData = {
+    labels: ['Charbon', 'Gaz Naturel', 'Hydroélectricité', 'Produits Pétroliers', "Nucléaire", "Biomasse", "Autres (géothermie, solaire, éolien,...)"],
+    datasets: [{
+      data: [300, 500, 100, 200, 500, 600, 700],
+      backgroundColor: [
+        colors["brand-color"],
+        colors["blue-color"],
+        colors["abricot-color"],
+        colors["green-color"],
+        colors["orange-color"],
+        colors["pink-color"],
+        colors["violet-color"]
+      ]
+    }]
+  };
+
+  public pieChartDataRegion2: ChartData = {
+    labels: ['Charbon', 'Gaz Naturel', 'Hydroélectricité', 'Produits Pétroliers', "Nucléaire", "Biomasse", "Autres (géothermie, solaire, éolien,...)"],
+    datasets: [{
+      data: [300, 700, 100, 400, 500, 600,200 ],
+      backgroundColor: [
+        colors["brand-color"],
+        colors["blue-color"],
+        colors["abricot-color"],
+        colors["green-color"],
+        colors["orange-color"],
+        colors["pink-color"],
+        colors["violet-color"]
+      ]
+    }]
+  };
+
 
 }
